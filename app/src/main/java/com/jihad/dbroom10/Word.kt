c5e99bb7b6d7b6aace327855c487bbe019e0136e
Saodah1202 <oodah.12@gmail.com>
2019-11-29 00:50:46 +0700

    first commit

---
+++ app/src/main/java/com/jihad/dbroom10/Word.kt
@@ -0,0 +1,6 @@
+package com.jihad.dbroom10
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName="word_table")
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
